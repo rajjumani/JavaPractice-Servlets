de3296f81bd7a37907b6278016dfe970367b0d93
Raj Jumani <Raj Jumani@10.1.246.207>
2019-06-13 02:31:13 -0400

    servlets init

---
+++ WebContent/login.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form name="frm1" action="Check" method="get">
+<table>
+
+  <tr>
+  <td><label>Username:</label></td>
+  <td><input type="text" name="uname"></td>
+  </tr>
+  
+  <tr>
+  <td><label>Password:</label></td>
+  <td><input type="password" name="pass"></td>
+  </tr>
+  
+  <tr>
+  <center><td rowspan="2"><input type="submit" value="Submit"></td></center>
+  </tr>
+  
+</table>
+</form>
+</body>
+</html>
---
+++ src/com/servletconfig/SerConfig.java
@@ -0,0 +1,46 @@
+package com.servletconfig;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SerConfig
+ */
+@WebServlet("/SerConfig")
+public class SerConfig extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SerConfig() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=response.getWriter();
+		ServletConfig config=getServletConfig();
+		String username=config.getInitParameter("username");
+		out.print(username);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
